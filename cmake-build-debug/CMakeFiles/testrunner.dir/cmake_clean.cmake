49d8c51cc334d4e8e034d34ab7c3f692bf5648d7
Glauco Acassio <acassioglauco@gmail.com>
2024-06-30 14:14:10 -0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/testrunner.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testrunner.dir/testrunner/main.cpp.o"
+  "CMakeFiles/testrunner.dir/testrunner/main.cpp.o.d"
+  "testrunner"
+  "testrunner.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testrunner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/testrunner.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for testrunner.
+# This may be replaced when dependencies are built.
